a10c0bbd933d2619a8839358e3aa418fcc435207
Akhilendra01 <akhilendra1604@gmail.com>
2025-02-26 01:35:15 +0530

    added: controllers for post in content service, fixed: mongo db warnings

---
+++ content-service/controllers/PostControllers.js
@@ -0,0 +1,43 @@
+const Post = require("../models/Post");
+
+async function createPost(req, res) {
+  const user = req.user;
+  const post = new Post({
+    author: user.username,
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  await post.save();
+
+  res.send({
+    status: 201,
+    message: "Post created successfully",
+  });
+}
+
+async function getPost(req, res) {
+  const post = await Post.findById(req.params.id);
+  res.send({
+    status: 200,
+    data: {
+      post: post,
+    },
+  });
+}
+
+async function deletePost(req, res) {
+  const post = await Post.findById(req.params.id);
+  if (post.author === req.user.username) {
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Post deleted successfully" });
+  } else {
+    res.status(403).json({ message: "Unauthorised" });
+  }
+}
+
+module.exports = {
+  createPost,
+  getPost,
+  deletePost,
+};
--- content-service/index.js
+++ content-service/index.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const morgan = require("morgan");
+const router = require("./router");
+const db=require('./db');
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(router);
 
 app.listen(process.env.PORT, () => {
   console.log(`Content service running on port [ ${process.env.PORT} ]`);
