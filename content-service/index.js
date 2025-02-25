72ef94fe295e35db63305d29d602a2bd08579e27
Akhilendra01 <akhilendra1604@gmail.com>
2025-02-26 01:43:07 +0530

    added: auth token verification in content-service

--- content-service/index.js
+++ content-service/index.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const router = require("./router");
-const db=require('./db');
+const db = require("./db");
+const verifyToken = require("./middlewares/token-verify");
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(verifyToken);
 app.use(router);
 
 app.listen(process.env.PORT, () => {
---
+++ content-service/middlewares/token-verify.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+
+async function verifyToken(req, res, next) {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Access Denied: No Token Provided" });
+    }
+
+    const response = await axios.post(
+      `${process.env.AUTH_SERVER_URL}/validate`,
+      {},
+      {
+        headers: { Authorization: token },
+      }
+    );
+    req.user = response.data.user;
+
+    next(); 
+  } catch (error) {
+    return res.status(403).json({
+      message: "Invalid Token",
+    });
+  }
+}
+
+module.exports = verifyToken;
