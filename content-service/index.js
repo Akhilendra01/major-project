16e6dd7485635102d47f15cb097b5750de1e8af9
Akhilendra01 <akhilendra1604@gmail.com>
2025-02-26 01:04:15 +0530

    created: content-service skeleton, post model

---
+++ content-service/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Content service running on port [ ${process.env.PORT} ]`);
+});
