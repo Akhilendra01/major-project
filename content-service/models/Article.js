35bd7cf8c2cbe7edefd5dce7e628e9b7215370c9
Akhilendra01 <akhilendra1604@gmail.com>
2025-03-13 23:34:20 +0530

    added: controllers for articles

---
+++ content-service/models/Article.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  companyTag: {
+    type: String,
+    required: true,
+  },
+  roleTag: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Article", articleSchema);
